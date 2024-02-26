b7ad869479fb5588b35f922c0f88882d57f1d9e4
dell <dell@192.168.1.5>
2024-02-26 19:40:03 +0800

    buh oorchlolt

--- buteelt/src/buteelt/Main1.java
+++ buteelt/src/buteelt/Main1.java
@@ -3,47 +3,53 @@ import java.util.Scanner;
 
 public class Main1 {
     public static void main(String[] args) {
-        MyQueue<Integer> queue = new MyQueue<>(100);
+        MyQueue<Object> queue = new MyQueue<>(10);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Та хэдэн элемент оруулах вэ? ");
+        System.out.println("Та хэдэн бүтээгдэхүүн оруулах вэ? ");
         int n = sc.nextInt();
+        sc.nextLine(); 
 
         for (int i = 1; i <= n; i++) {
-            queue.enqueue(sc.nextInt());
+            char b = sc.nextLine().charAt(0);
+            queue.enqueue(b);
         }
 
         boolean swtch = true;
         while (swtch) {
-            System.out.println("\n1)Дараалалаас элэмент устгах()\n"
-                    + "2) дараалалад элемент нэмэх()\n"
-                    + "3) хамгийн эхний элемэнт()\n"
-                    + "4) хамгийн хойд талийн элемент()\n"
-                    + "5) дараалал дахь нийт элемэнтийн тоо()\n"
-                    + "6) үлдсэн дараалалийн тоо()\n");
+            System.out.println("\n1) Дарааллаас элэмент устгах()\n"
+                    + "2) Дараалалд элемент нэмэх()\n"
+                    + "3) Хамгийн эхний элемент()\n"
+                    + "4) Хамгийн сүүлийн элемент()\n"
+                    + "5) Дараалал дахь нийт элементийн тоо()\n"
+                    + "6) Дараалалд байгаа элементүүдийн тоо()\n"
+                    + "7) Жагсаалт харах\n");
 
             switch (sc.nextInt()) {
                 case 1:
-                    System.out.println("\n" + queue.dequeue() + "Дараалалаас хасагдсан элемент\n");
+                    System.out.println("\n" + queue.dequeue() + " Дарааллаас хасагдсан элемент\n");
                     break;
                 case 2:
-                    queue.enqueue(sc.nextInt());
-                    System.out.println("\n" + queue.rear() + " дараалалд нэмэгдсэн элемент\n");
+                    char element = sc.next().charAt(0);
+                    queue.enqueue(element);
+                    System.out.println("\n" + element + " Дараалалд нэмэгдсэн элемент\n");
                     break;
                 case 3:
                     System.out.println("Хамгийн эхний элемент: " + queue.front());
                     break;
                 case 4:
-                    System.out.println("хамгийн сүүлийн элемент: " + queue.rear());
+                    System.out.println("Хамгийн сүүлийн элемент: " + queue.rear());
                     break;
                 case 5:
-                    System.out.println("дараалал дахь элементийн тоо :" + queue.size());
+                    System.out.println("Дараалал дахь элементийн тоо: " + queue.size());
                     break;
                 case 6:
-                    System.out.println("үлдсэн элементийн тоо: " + queue.remainingCapacity());
+                    System.out.println("Үлдсэн элементийн тоо: " + queue.remainingCapacity());
                     break;
-
-             
+                case 7:
+                    System.out.println("Дарааллын хувийн жагсаалт: ");
+                    queue.display();
+                    break;
                 default:
                     swtch = false;
                     break;
@@ -54,7 +60,7 @@ public class Main1 {
     static class MyQueue<T> {
         int front, rear, size;
         int capacity;
-        T array[];
+        T[] array;
 
         public MyQueue(int capacity) {
             this.capacity = capacity;
@@ -62,6 +68,7 @@ public class Main1 {
             this.rear = capacity - 1;
             this.array = (T[]) new Object[this.capacity];
         }
+        
 
         boolean isFull() {
             return (size == capacity);
@@ -71,15 +78,15 @@ public class Main1 {
             return (size == 0);
         }
 
-        void enqueue(T item) {
+        void enqueue(T b) {
             if (isFull()) {
                 System.out.println("Дараалал дүүрэн байна");
                 return;
             }
             rear = (rear + 1) % capacity;
-            array[rear] = item;
+            array[rear] = b;
             size++;
-            System.out.println(item + " дараалалд орсон");
+            System.out.println(b + " Дараалалд орсон");
         }
 
         T dequeue() {
@@ -113,6 +120,21 @@ public class Main1 {
 
         int remainingCapacity() {
             return capacity - size;
+        }
+
+        // Method to display the contents of the queue
+        void display() {
+            if (isEmpty()) {
+                System.out.println("Дараалал хоосон байна");
+                return;
+            }
+            System.out.println("Дарааллын хувийн жагсаалт:");
+            int i = front;
+            while (i != rear) {
+                System.out.print(array[i] + " ");
+                i = (i + 1) % capacity;
+            }
+            System.out.println(array[rear]);
         }
     }
 }
---
+++ buteelt/src/buteelt/iNeedGeneric.java
@@ -0,0 +1,16 @@
+package buteelt;
+public class iNeedGeneric {
+    private Integer value;
+
+    public iNeedGeneric(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
